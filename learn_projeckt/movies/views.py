0c5744d699001d3584e9fdcc94fd36c1c8ab71fd
Nikolay <odna_shestaia@mail.ru>
2022-10-10 19:01:27 +0300

    commit

---
+++ learn_projeckt/movies/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+
+from .models import *
+
+
+class MoviesView(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movies.html", {'movie_list': movies})
